fb0f0b275fde01678e4ddaa40ba369c0f3244e84
Kevin Rise <krise@veracode.com>
2020-10-01 07:58:18 -0400

    used named params instead of positional

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,56 @@
 #!/bin/sh -l
 
-appname=$1
-createprofile=$2
-filepath=$3
-version=$4
-vid=$5
-vkey=$6
+# default to false, so as to not create a new App Profile
+createprofile=false
+
+# parse the inputs
+while :; do
+     echo $1
+     echo $2
+     echo checking...
+     case $1 in
+          -appname) 
+               appname=$2
+               shift
+               ;;
+          -createprofile)
+               if [ $2 = "true" ]; then
+                    createprofile=true
+               fi
+               shift
+               ;;
+          -filepath)
+               filepath=$2
+               shift
+               ;;
+          -scan_name)
+               scan_name=$2
+               shift
+               ;;
+          -vid)
+               vid=$2
+               shift
+               ;;
+          -vkey)
+               vkey=$2
+               shift
+               ;;
+          -opt_args)
+               opt_args=$2
+               shift
+               ;;
+          *)
+               break
+     esac
+     shift
+done
+
 
 echo "appname: $appname"
 echo "createprofile: $createprofile"
 echo "filepath: $filepath"
-echo "version: $version"
+echo "scan_name: $scan_name"
+echo "optional args: $opt_args"
 
 #below pulls latest wrapper version. alternative is to pin a version like so:
 #javawrapperversion=20.8.7.1
@@ -19,13 +59,13 @@ javawrapperversion=$(curl https://repo1.maven.org/maven2/com/veracode/vosp/api/w
 
 echo "javawrapperversion: $javawrapperversion"
 
-curl -sS -o VeracodeJavaAPI.jar "https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/$javawrapperversion/vosp-api-wrappers-java-$javawrapperversion.jar"
-java -jar VeracodeJavaAPI.jar \
-     -action UploadAndScan \
-     -appname "$appname" \
-     -createprofile "$createprofile" \
-     -filepath "$filepath" \
-     -version "$version" \
-     -vid "$vid" \
-     -vkey "$vkey" \
-     -autoscan true
+# curl -sS -o VeracodeJavaAPI.jar "https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/$javawrapperversion/vosp-api-wrappers-java-$javawrapperversion.jar"
+# java -jar VeracodeJavaAPI.jar \
+#      -action UploadAndScan \
+#      -appname "$appname" \
+#      -createprofile "$createprofile" \
+#      -filepath "$filepath" \
+#      -version "$scan_name" \
+#      -vid "$vid" \
+#      -vkey "$vkey" \
+#      -autoscan true $opt_args
