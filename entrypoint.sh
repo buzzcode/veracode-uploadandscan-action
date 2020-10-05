06b1f0606705f9cb82dad23c3117d71a408dff5b
Kevin Rise <krise@veracode.com>
2020-10-05 10:36:57 -0400

    test: test_data\hello.jar

--- entrypoint.sh
+++ entrypoint.sh
@@ -69,14 +69,14 @@ fi
 javawrapperversion=$(curl https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/maven-metadata.xml | grep latest |  cut -d '>' -f 2 | cut -d '<' -f 1)
 echo "javawrapperversion: $javawrapperversion"
 
-# curl -sS -o VeracodeJavaAPI.jar "https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/$javawrapperversion/vosp-api-wrappers-java-$javawrapperversion.jar"
-# java -jar VeracodeJavaAPI.jar \
-#      -action UploadAndScan \
-#      -appname "$appname" \
-#      -createprofile "$createprofile" \
-#      -filepath "$filepath" \
-#      -version "$scan_name" \
-#      -vid "$vid" \
-#      -vkey "$vkey" \
-#      -autoscan true \
-#      $opt_args
+curl -sS -o VeracodeJavaAPI.jar "https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/$javawrapperversion/vosp-api-wrappers-java-$javawrapperversion.jar"
+java -jar VeracodeJavaAPI.jar \
+     -action UploadAndScan \
+     -appname $appname \
+     -createprofile $createprofile \
+     -filepath $filepath \
+     -version $scan_name \
+     -vid $vid \
+     -vkey $vkey \
+     -autoscan true \
+     $opt_args
