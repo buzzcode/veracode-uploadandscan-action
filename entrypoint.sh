81dcf1e678f13772293d4daf039e3216e19331d9
Kevin Rise <krise@veracode.com>
2020-10-05 10:48:03 -0400

    echo jar

--- entrypoint.sh
+++ entrypoint.sh
@@ -70,7 +70,7 @@ javawrapperversion=$(curl https://repo1.maven.org/maven2/com/veracode/vosp/api/w
 echo "javawrapperversion: $javawrapperversion"
 
 curl -sS -o VeracodeJavaAPI.jar "https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/$javawrapperversion/vosp-api-wrappers-java-$javawrapperversion.jar"
-java -jar VeracodeJavaAPI.jar \
+echo java -jar VeracodeJavaAPI.jar \
      -action UploadAndScan \
      -appname $appname \
      -createprofile $createprofile \
