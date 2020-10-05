ebb15674221367bef38bf50b579b32a5c5a25dff
Kevin Rise <krise@veracode.com>
2020-10-05 10:59:01 -0400

    really do it

--- entrypoint.sh
+++ entrypoint.sh
@@ -73,7 +73,18 @@ ls -l
 ls -l $filepath
 
 curl -sS -o VeracodeJavaAPI.jar "https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/$javawrapperversion/vosp-api-wrappers-java-$javawrapperversion.jar"
-echo java -jar VeracodeJavaAPI.jar \
+echo [Calling]: java -jar VeracodeJavaAPI.jar \
+     -action UploadAndScan \
+     -appname \"$appname\" \
+     -createprofile $createprofile \
+     -filepath \"$filepath\" \
+     -version \"$scan_name\" \
+     -vid \"$vid\" \
+     -vkey \"$vkey\" \
+     -autoscan true \
+     $opt_args
+
+java -jar VeracodeJavaAPI.jar \
      -action UploadAndScan \
      -appname \"$appname\" \
      -createprofile $createprofile \
